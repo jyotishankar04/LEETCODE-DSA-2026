329fd0f34d581ab6a13672d6174f81883c2bb1d1
Jyotishankar Patra <patrajyotishankar@gmail.com>
2026-08-28 00:14:23 +0530

    Time: 4 ms (100%), Space: 45.6 MB (94.11%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int copy = x;
+        int rev = 0;
+        while(copy > 0){
+            int ld = copy % 10;
+            rev = (rev * 10 + ld);
+            copy/=10;
+        }
+        return rev == x;
+    }
+}
